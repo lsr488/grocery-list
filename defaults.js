cad487a036c0f505bb8faf188694087421bdeb46
unknown <lsr488@gmail.com>
2020-12-13 19:14:29 -0500

    initial commit

---
+++ defaults.js
@@ -0,0 +1,57 @@
+// CHANGE THIS
+const defaultItems = [
+	 {
+		"item": "half and half", 
+		 	"state": false, 
+ 		},
+	 {
+		"item": "milk", 
+		 	"state": false, 
+ 		},
+	 {
+		"item": "walnuts", 
+		 	"state": false, 
+ 		},
+	 {
+		"item": "yellow onion (soup)", 
+		 	"state": false, 
+ 		},
+	 {
+		"item": "1lb ground Italian sausage (soup)", 
+		 	"state": false, 
+ 		},
+	 {
+		"item": "carrots (soup)", 
+		 	"state": false, 
+ 		},
+	 {
+		"item": "sage (soup)", 
+		 	"state": false, 
+ 		},
+	 {
+		"item": "thyme (soup)", 
+		 	"state": false, 
+ 		},
+	 {
+		"item": "mustard powder (soup)", 
+		 	"state": false, 
+ 		},
+	 {
+		"item": "10oz spinach (fresh or frozen) (soup)", 
+		 	"state": false, 
+ 		},
+	]
+
+// CHANGE THIS
+const defaultRecipes = [
+		{ 
+			"url": "", 
+			"name": "Russian tea cakes", 
+			"notes": "",  
+		},
+		{ 
+			"url": "https://workweeklunch.com/recipe/italian-sausage-and-gnocchi-soup/", 
+			"name": "Italian Sausage & Gnocchi soup", 
+			"notes": "stock not broth; tortellini not gnocchi",  
+		},
+]
