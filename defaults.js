276acb79c197dea36bd0bebf92bee3bde63db807
unknown <lsr488@gmail.com>
2021-01-01 16:55:10 -0500

    Updated defaults.

--- defaults.js
+++ defaults.js
@@ -1,53 +1,5 @@
 // CHANGE THIS
 const defaultItems = [
-	 {
-		"item": "2 yellow onion (soup & meatloaf)", 
-		 	"state": false, 
- 		},
-	 {
-		"item": "1lb ground Italian sausage (soup)", 
-		 	"state": false, 
- 		},
-	 {
-		"item": "3 carrots (soup & meatloaf)", 
-		 	"state": false, 
- 		},
-	 {
-		"item": "sage (soup)", 
-		 	"state": false, 
- 		},
-	 {
-		"item": "thyme (soup)", 
-		 	"state": false, 
- 		},
-	 {
-		"item": "mustard powder (soup)", 
-		 	"state": false, 
- 		},
-	 {
-		"item": "10oz spinach (fresh or frozen) (soup)", 
-		 	"state": false, 
- 		},
-	 {
-		"item": "half & half", 
-		 	"state": false, 
- 		},
-	 {
-		"item": "4 yukon gold potatoes (meatloaf & veggies)", 
-		 	"state": false, 
- 		},
-	 {
-		"item": "1 carrot (meatloaf & veggies)", 
-		 	"state": false, 
- 		},
-	 {
-		"item": "dijon mustard (meatloaf & veggies)", 
-		 	"state": false, 
- 		},
-	 {
-		"item": "asparagus (meatloaf & veggies)", 
-		 	"state": false, 
- 		},
 		{
 			"item": "half and half", 
 		 	"state": false, 
@@ -65,23 +17,8 @@ const defaultItems = [
 // CHANGE THIS
 const defaultRecipes = [
 		{ 
-			"url": "https://wwlprogram.com/#/recipes/194", 
-			"name": "Beet, chickpea salad", 
-			"notes": "sub quinoa out wild rice for farro; sub parmesan in fridge; sub walnuts and raisins for pepitas and cranberries ",  
-		},
-		{ 
-			"url": "https://workweeklunch.com/recipe/italian-sausage-and-gnocchi-soup/", 
-			"name": "Italian Sausage & Gnocchi soup", 
-			"notes": "stock not broth; tortellini not gnocchi; use Italian blend seasoning instead of individual spices; use mustard instead of mustard powder",  
-		},
-		{ 
-			"url": "https://www.allrecipes.com/recipe/22730/rocky-road-candies/", 
-			"name": "rocky road ", 
-			"notes": "",  
-		},
-		{ 
-			"url": "https://wwlprogram.com/#/recipes/147", 
-			"name": "Meatloaf Cups, Veggie, and Mashed Potatoes", 
+			"url": "", 
+			"name": "", 
 			"notes": "",  
 		},
 ]
