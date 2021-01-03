fded37513c844e738611e7e18e9293ca44653448
unknown <lsr488@gmail.com>
2021-01-03 18:00:45 -0500

    Updated defaults.

--- defaults.js
+++ defaults.js
@@ -1,24 +1,88 @@
 // CHANGE THIS
 const defaultItems = [
-		{
-			"item": "half and half", 
-		 	"state": false, 
- 		},
-	 {
-		"item": "milk", 
-		 	"state": false, 
- 		},
-	 {
-		"item": "walnuts", 
-		 	"state": false, 
- 		}
-	]
+ {
+    "item": "half & half", 
+         "state": false, 
+     },
+ {
+    "item": "2 red bell pepper (salmon & veg, pizza)", 
+         "state": false, 
+     },
+ {
+    "item": "red cabbage (salmon & veg)", 
+         "state": false, 
+     },
+ {
+    "item": "3 sweet potatoes (salmon & veg)", 
+         "state": false, 
+     },
+ {
+    "item": "low-moisture mozarella cheese (pizza)", 
+         "state": false, 
+     },
+ {
+    "item": "sliced black olives (pizza)", 
+         "state": false, 
+     },
+ {
+    "item": "8 oz mushrooms (pizza)", 
+         "state": false, 
+     },
+ {
+    "item": "pepperoni or prosciutto (pizza)", 
+         "state": false, 
+     },
+ {
+    "item": "2 limes (curry)", 
+         "state": false, 
+     },
+ {
+    "item": "1 inch ginger (curry)", 
+         "state": false, 
+     },
+ {
+    "item": "30 oz canned coconut milk (curry)", 
+         "state": false, 
+     },
+ {
+    "item": "1-2 carrots (curry)", 
+         "state": false, 
+     },
+ {
+    "item": "1 zucchini (curry)", 
+         "state": false, 
+     },
+ {
+    "item": "1 cauliflower (curry)", 
+         "state": false, 
+     },
+]
 
 // CHANGE THIS
 const defaultRecipes = [
-		{ 
-			"url": "#", 
-			"name": "Delicious Recipe", 
-			"notes": "",  
-		},
+  { 
+      "url": "", 
+      "name": "salmon & veggies", 
+      "notes": "",  
+  },
+  { 
+      "url": "https://wwlprogram.com/#/recipes/233", 
+      "name": "Thai coconut curry ", 
+      "notes": "replace veggies",  
+  },
+  { 
+      "url": "https://www.allrecipes.com/recipe/22730/rocky-road-candies/", 
+      "name": "rocky road ", 
+      "notes": "",  
+  },
+  { 
+      "url": "https://wwlprogram.com/#/recipes/566", 
+      "name": "Green enchiladas ", 
+      "notes": "",  
+  },
+  { 
+      "url": "", 
+      "name": "Sourdough pizza ", 
+      "notes": "olives, cheese, mushrooms, bell pepper, pepperoni/prosciutto",  
+  },
 ]
