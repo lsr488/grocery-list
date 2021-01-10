84e17eeab618dda39be2ffee1e9d717ba4eca31c
unknown <lsr488@gmail.com>
2021-01-10 12:53:24 -0500

    Updated defaults.

--- defaults.js
+++ defaults.js
@@ -1,88 +1,57 @@
 // CHANGE THIS
 const defaultItems = [
- {
-    "item": "half & half", 
-         "state": false, 
-     },
- {
-    "item": "2 red bell pepper (salmon & veg, pizza)", 
-         "state": false, 
-     },
- {
-    "item": "red cabbage (salmon & veg)", 
-         "state": false, 
-     },
- {
-    "item": "3 sweet potatoes (salmon & veg)", 
-         "state": false, 
-     },
- {
-    "item": "low-moisture mozarella cheese (pizza)", 
-         "state": false, 
-     },
- {
-    "item": "sliced black olives (pizza)", 
-         "state": false, 
-     },
- {
-    "item": "8 oz mushrooms (pizza)", 
-         "state": false, 
-     },
- {
-    "item": "pepperoni or prosciutto (pizza)", 
-         "state": false, 
-     },
- {
-    "item": "2 limes (curry)", 
-         "state": false, 
-     },
- {
-    "item": "1 inch ginger (curry)", 
-         "state": false, 
-     },
- {
-    "item": "30 oz canned coconut milk (curry)", 
-         "state": false, 
-     },
- {
-    "item": "1-2 carrots (curry)", 
-         "state": false, 
-     },
- {
-    "item": "1 zucchini (curry)", 
-         "state": false, 
-     },
- {
-    "item": "1 cauliflower (curry)", 
-         "state": false, 
-     },
+  {
+    "item": "eggs", 
+    "state": false, 
+  },
+  {
+    "item": "tampons", 
+    "state": false, 
+  },
+  {
+    "item": "brown sugar", 
+    "state": false, 
+  },
+  {
+    "item": "regular sugar", 
+    "state": false, 
+  },
+  {
+    "item": "1 lb chicken breasts (bourbon chicken)", 
+    "state": false, 
+  },
+  {
+    "item": "apple cider vinegar (bourbon chicken)", 
+    "state": false, 
+  },
+  {
+    "item": "1 carrot (bourbon chicken)", 
+    "state": false, 
+  },
+  {
+    "item": "1 red bell pepper (bourbon chicken)", 
+    "state": false, 
+  },
+  {
+    "item": "frozen peas (bourbon chicken)", 
+    "state": false, 
+  },
+  {
+    "item": "sour cream (beef stroganoff)", 
+    "state": false, 
+  },
 ]
 
 // CHANGE THIS
 const defaultRecipes = [
   { 
-      "url": "", 
-      "name": "salmon & veggies", 
-      "notes": "",  
+    "url": "https://wwlprogram.com/#/recipes/47", 
+    "name": "Bourbon-ish Chicken", 
+    "notes": "",  
   },
   { 
-      "url": "https://wwlprogram.com/#/recipes/233", 
-      "name": "Thai coconut curry ", 
-      "notes": "replace veggies",  
-  },
-  { 
-      "url": "https://www.allrecipes.com/recipe/22730/rocky-road-candies/", 
-      "name": "rocky road ", 
-      "notes": "",  
-  },
-  { 
-      "url": "https://wwlprogram.com/#/recipes/566", 
-      "name": "Green enchiladas ", 
-      "notes": "",  
-  },
-  { 
-      "url": "", 
-      "name": "Sourdough pizza ", 
-      "notes": "olives, cheese, mushrooms, bell pepper, pepperoni/prosciutto",  
+    "url": "https://www.budgetbytes.com/one-pot-beef-mushroom-stroganoff/", 
+    "name": "One Pot Beef Stroganoff", 
+    "notes": "",  
   },
 ]
