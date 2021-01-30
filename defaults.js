3f0b56d5500c44a5d6d8ed36a5d50d90f0f78245
unknown <lsr488@gmail.com>
2021-01-30 15:24:35 -0500

    Updated defaults.

--- defaults.js
+++ defaults.js
@@ -8,59 +8,7 @@ const defaultItems = [
   {
     "item": "regular sugar", 
     "state": false, 
-  },
-  {
-    "item": "mission medium flour tortillas, 8-10 (tacos)", 
-    "state": false, 
-  },
-  {
-    "item": "1 small onion (tacos)", 
-    "state": false, 
-  },
-  {
-    "item": "1 small red pepper (tacos)", 
-    "state": false, 
-  },
-  {
-    "item": "1 small can of corn (tacos)", 
-    "state": false, 
-  },
-  {
-    "item": "1.25 oz taco seasoning packet (with starch) (tacos)", 
-    "state": false, 
-  },
-  {
-    "item": "8 oz sour cream (tacos)", 
-    "state": false, 
-  },
-  {
-    "item": "limes (tacos)", 
-    "state": false, 
-  },
-  {
-    "item": "olive oil", 
-    "state": false, 
-  },
-  {
-    "item": "eggs", 
-    "state": false, 
-  },
-  {
-    "item": "1 acorn squash (winter grain bowl)", 
-    "state": false, 
-  },
-  {
-    "item": "1 leek (winter grain bowl)", 
-    "state": false, 
-  },
-  {
-    "item": "2 carrots (winter grain bowl)", 
-    "state": false, 
-  },
-  {
-    "item": "feta cheese (winter grain bowl)", 
-    "state": false, 
-  },
+  }
 ]
 // RECIPE OUTPUT
 const defaultRecipes = [
@@ -68,11 +16,6 @@ const defaultRecipes = [
     "url": "https://wwlprogram.com/#/recipes/561", 
     "name": "Winter Grain Bowl", 
     "notes": "rice instead of barley",  
-  },
-  { 
-    "url": "https://www.dropbox.com/s/px3nn2zl9iy5lym/AdamRaguseasLentiltacoswithlikecrema_210119_074726.pdf?dl=0", 
-    "name": "Adam Ragusea's lentil tacos", 
-    "notes": "",  
-  },
+  }
 ]
 
