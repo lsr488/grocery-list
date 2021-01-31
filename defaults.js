8c3af4e6c99ae8086b66c9e38eb45d86bd9bd203
unknown <lsr488@gmail.com>
2021-01-30 19:23:34 -0500

    Updated defaults.

--- defaults.js
+++ defaults.js
@@ -1,26 +1,88 @@
-// 01-24-2021 11:19
+// 01-30-2021 07:22
 // ITEM OUTPUT:
 const defaultItems = [
   {
-    "item": "half & half", 
+    "item": "regular sugar", 
     "state": false, 
   },
   {
-    "item": "regular sugar", 
+    "item": "brussel sprouts (sheet pan)", 
     "state": false, 
-  }
-]
-// RECIPE OUTPUT
-const defaultRecipes = [
-  { 
-    "url": "https://wwlprogram.com/#/recipes/561", 
-    "name": "Winter Grain Bowl", 
-    "notes": "rice instead of barley",  
   },
-  { 
-    "url": "#", 
-    "name": "test recipe", 
-    "notes": "test notes",  
+  {
+    "item": "sweet potatoes (sheet pan)", 
+    "state": false, 
+  },
+  {
+    "item": "butter", 
+    "state": false, 
+  },
+  {
+    "item": "zucchini (muffins)", 
+    "state": false, 
+  },
+  {
+    "item": "chocolate chips (2 bags)", 
+    "state": false, 
+  },
+  {
+    "item": "mini marshmallows (rocky road)", 
+    "state": false, 
+  },
+  {
+    "item": "peanuts (rocky road)", 
+    "state": false, 
+  },
+  {
+    "item": "1lb chicken breast (chow mein)", 
+    "state": false, 
+  },
+  {
+    "item": "soy sauce (chow mein)", 
+    "state": false, 
+  },
+  {
+    "item": "green onions (chow mein)", 
+    "state": false, 
+  },
+  {
+    "item": "8 oz mushrooms (chow mein)", 
+    "state": false, 
+  },
+  {
+    "item": "1 head broccoli (chow mein)", 
+    "state": false, 
+  },
+  {
+    "item": "3 carrots (chow mein)", 
+    "state": false, 
+  },
+  {
+    "item": "6 oz thin Asian-style noodles (chow mein)", 
+    "state": false, 
   },
 ]
 
+// RECIPE OUTPUT
+const defaultRecipes = [
+  { 
+    "url": "", 
+    "name": "sausage & veggie sheet pan", 
+    "notes": "",  
+  },
+  { 
+    "url": "https://wwlprogram.com/#/recipes/218", 
+    "name": "chow mein", 
+    "notes": "",  
+  },
+  { 
+    "url": "https://wwlprogram.com/#/recipes/320", 
+    "name": "chocolate zucchini muffins", 
+    "notes": "",  
+  },
+  { 
+    "url": "https://www.dropbox.com/s/j3w0tl6qjf610qm/Rocky%20Road%20Candies%20Recipe%20_%20Allrecipes.PDF?dl=0", 
+    "name": "Rocky Road again", 
+    "notes": "",  
+  },
+]
