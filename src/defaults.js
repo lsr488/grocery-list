f365ce4117941043e6521f6c5552a6007f52947a
unknown <lsr488@gmail.com>
2021-02-17 12:58:17 -0500

    Updated defaults

--- src/defaults.js
+++ src/defaults.js
@@ -1,50 +1,68 @@
-// 02-17-2021 11:20
+// 02-17-2021 00:57
+// ITEM OUTPUT:
 const defaultItems = [
   {
-    "item": "15 oz canned chickpeas ,,", 
+    "item": "15 oz canned chickpeas (wrap)", 
     "state": false, 
   },
   {
-    "item": "red curry paste ,,", 
+    "item": "red curry paste (tacos)", 
     "state": false, 
   },
   {
-    "item": "limes,,", 
+    "item": "limes (tacos)", 
     "state": false, 
   },
   {
-    "item": "9oz coleslaw mix,,", 
+    "item": "9oz coleslaw mix (tacos)", 
     "state": false, 
   },
   {
-    "item": "2 avocados,,", 
+    "item": "2 avocados (tacos)", 
     "state": false, 
   },
   {
-    "item": "fresh spinach ,,", 
+    "item": "fresh spinach (tacos)", 
     "state": false, 
   },
   {
-    "item": "small flour tortillas,,", 
+    "item": "small flour tortillas (wrap)", 
     "state": false, 
   },
   {
-    "item": "5 oz plain greek yogurt ,,", 
+    "item": "5 oz plain greek yogurt (wrap)", 
     "state": false, 
   },
   {
-    "item": "lemon,,", 
+    "item": "lemon (wrap)", 
     "state": false, 
   },
   {
-    "item": "toilet paper,,", 
+    "item": "toilet paper", 
     "state": false, 
   },
   {
-    "item": "half & half,,", 
+    "item": "half & half", 
+    "state": false, 
+  },
+  {
+    "item": "1 inch ginger (kung pao)", 
+    "state": false, 
+  },
+  {
+    "item": "green onions (kung pao)", 
+    "state": false, 
+  },
+  {
+    "item": "1 red bell pepper (kung pao)", 
+    "state": false, 
+  },
+  {
+    "item": "12 oz shrimp (kung pao)", 
     "state": false, 
   },
 ]
+// RECIPE OUTPUT
 const defaultRecipes = [
   { 
     "url": "https://wwlprogram.com/#/recipes/569", 
@@ -55,6 +73,11 @@ const defaultRecipes = [
     "url": "https://wwlprogram.com/#/recipes/289", 
     "name": "chickpea and taking sauce wrap", 
     "notes": "use jarred red pepper instead of fresh",  
+  },
+  { 
+    "url": "https://wwlprogram.com/#/recipes/584", 
+    "name": "kung pao noodle stir fry", 
+    "notes": "use rice instead of noodles?",  
   },
   { 
     "url": "https://www.dropbox.com/s/j3w0tl6qjf610qm/Rocky%20Road%20Candies%20Recipe%20_%20Allrecipes.PDF?dl=0", 
