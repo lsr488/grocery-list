adc4cb568bc8e84de0a5e169d72d4a4944ae9e5f
unknown <lsr488@gmail.com>
2021-02-20 18:15:14 -0500

    Removed Button class to own file.

--- src/items.js
+++ src/items.js
@@ -15,73 +15,6 @@ const itemInput = document.getElementById('item');
 const itemButton = document.getElementById('item-button');
 const newItems = document.getElementById('new-item');
 
-class Button {
-	constructor(elementType, column, type, name, item, eventType) {
-		this.elementType = elementType;
-		this.column = column;
-		this.type = type;
-		this.name = name;
-		this.item = item;
-		this.itemId = item.id;
-		this.element = this.createElement();
-
-		// console.log("eventType:", eventType);
-		// debugger
-
-		if(eventType == saveBtnClicked) {
-			console.log("save eventType");
-	  	this.element.addEventListener('click', this.save.bind(this));  	
-	  } else if (eventType) {
-		  this.element.addEventListener('click', eventType);
-	  } else {
-	  	this.element.addEventListener('click', this.edit.bind(this));
-	  }
-	}
-
-	createElement() {
-		const button = document.createElement(this.elementType);
-		button.classList.add('btn');
-		button.classList.add(this.column);
-		button.classList.add(this.type);
-		button.textContent = this.name;	
-		button.setAttribute('data-id', this.itemId);
-		return button;		
-	}
-
-	hide() {
-		this.element.classList.add("hidden");
-		return this.element;
-	}
-
-	edit() {
-		console.log("Button edit this:", this);
-		console.log('Button edit this.item:', this.item);
-
-		this.item.editingItem();
-
-		const saveBtn = document.querySelector(`.save[data-id="${this.item.id}"]`);
-		const deleteBtn = document.querySelector(`.delete[data-id="${this.item.id}"]`);
-
-		saveBtn.classList.remove("hidden");
-		deleteBtn.classList.add("hidden");
-		this.hide();
-	}
-
-	save() {
-		console.log("Button save this:", this);
-		console.log('Button save this.item:', this.item);
-
-		this.item.savingItem();
-
-		const editBtn = document.querySelector(`.edit[data-id="${this.item.id}"]`);
-		const deleteBtn = document.querySelector(`.delete[data-id="${this.item.id}"]`);
-
-		editBtn.classList.remove("hidden");
-		deleteBtn.classList.remove("hidden");
-		this.hide();
-
-	}
-}
 
 // checks if localStorage exists, creates from default array if not, or updates from localStorage if it does
 if(items.length === 0) {
@@ -154,11 +87,11 @@ function saveBtnClicked(e) {
 }
 
 function deleteBtnClicked(e) {
-	let button = e.target;
-	let _id = e.target.dataset.id;
-	const item = items[_id];
+	// let button = e.target;
+	// let _id = e.target.dataset.id;
+	// const item = items[_id];
 
-	deleteLocalStorageItem(items, item);
+	// deleteLocalStorageItem(items, item);
 
-	window.location.reload();
+	// window.location.reload();
 }
