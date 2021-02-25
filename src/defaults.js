f2fea707dfe26f0cb968a78612826e7da71ee892
unknown <lsr488@gmail.com>
2021-02-25 08:58:20 -0500

    Updated defaults

--- src/defaults.js
+++ src/defaults.js
@@ -1,88 +1,60 @@
-// 02-24-2021 03:36
+// 02-25-2021 08:56
 // ITEM OUTPUT:
 const defaultItems = [
   {
-    "item": "toilet paper", 
+    "name": "toilet paper", 
     "state": false, 
   },
   {
-    "item": "cumin", 
+    "name": "cumin", 
     "state": false, 
   },
   {
-    "item": "1 zucchini (red lentil curry)", 
+    "name": "1 zucchini (red lentil curry)", 
     "state": false, 
   },
   {
-    "item": "frozen peas? (red lentil curry)", 
+    "name": "frozen peas? (red lentil curry)", 
     "state": false, 
   },
   {
-    "item": "15 oz coconut milk (red lentil curry)", 
+    "name": "15 oz coconut milk (red lentil curry)", 
     "state": false, 
   },
   {
-    "item": "lentils? (red lentil curry)", 
+    "name": "lentils? (red lentil curry)", 
     "state": false, 
   },
   {
-    "item": "garam masala? (red lentil curry)", 
+    "name": "garam masala? (red lentil curry)", 
     "state": false, 
   },
   {
-    "item": "turmeric (red lentil curry)", 
+    "name": "turmeric (red lentil curry)", 
     "state": false, 
   },
   {
-    "item": "serrano pepper (red lentil curry)", 
+    "name": "serrano pepper (red lentil curry)", 
     "state": false, 
   },
   {
-    "item": "2 limes (red lentil curry)", 
+    "name": "2 limes (red lentil curry)", 
     "state": false, 
   },
   {
-    "item": "1 onion (burrito pasta bowl)", 
+    "name": "2 carrots (sheet pan)", 
     "state": false, 
   },
   {
-    "item": "1lb ground meat (burrito pasta bowl)", 
+    "name": "4 turnips (sheet pan)", 
     "state": false, 
   },
   {
-    "item": "short pasta (burrito pasta bowl)", 
+    "name": "4 potatoes (sheet pan)", 
     "state": false, 
   },
   {
-    "item": "15 oz can diced tomatoes (burrito pasta bowl)", 
-    "state": false, 
-  },
-  {
-    "item": "taco seasoning packet? (burrito pasta bowl)", 
-    "state": false, 
-  },
-  {
-    "item": "1 red bell pepper (burrito pasta bowl)", 
-    "state": false, 
-  },
-  {
-    "item": "11 oz can of corn (burrito pasta bowl)", 
-    "state": false, 
-  },
-  {
-    "item": "15 oz can black beans? (burrito pasta bowl)", 
-    "state": false, 
-  },
-  {
-    "item": "1 cup shredded cheese (burrito pasta bowl)", 
-    "state": false, 
-  },
-  {
-    "item": "sliced black olives (burrito pasta bowl)", 
-    "state": false, 
-  },
-  {
-    "item": "1 jalapeno (burrito pasta bowl)", 
+    "name": "4 beets (sheet pan)", 
     "state": false, 
   },
 ]
@@ -99,9 +71,9 @@ const defaultRecipes = [
     "notes": "incorporate leftover coconut cream",  
   },
   { 
-    "url": "https://wwlprogram.com/#/recipes/347", 
-    "name": "Cheesy Burrito Bowl Pasta", 
-    "notes": "",  
+    "url": "https://wwlprogram.com/#/recipes/570", 
+    "name": "Veggie sheet pan", 
+    "notes": "carrots, turnips, potatoes, beets",  
   },
 ]
 
