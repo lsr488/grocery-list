e7901cc3122d93ca9305c3cb42debc4a6cd17cd3
unknown <lsr488@gmail.com>
2021-03-20 09:43:41 -0400

    Initial commit

---
+++ src/Item.js
@@ -0,0 +1,70 @@
+class Item {
+	constructor(itemId, name, state) {
+		this.id = itemId;
+		this.name = name;
+		this.state = state;
+		this.element = this.createElement();
+
+		// these three options for savedListener all work to remove the eventListern
+		// this.savedListener = () => this.toggleChecked();
+		// this.savedListener = function() { this.toggleChecked() };
+		this.savedListener = this.toggleChecked.bind(this);
+
+		this.element.addEventListener('click', this.savedListener, false);
+	}
+
+	createElement() {
+		const item = document.createElement('li');
+		item.setAttribute('id', this.id);
+		item.classList.add('col1');
+		item.textContent = this.name;
+		return item;
+	}
+
+	setId() {}
+
+	addItem() {
+		items.push(this);
+		updateLocalStorage("items", items);
+	}
+
+	toggleChecked() {
+	 	this.element.classList.toggle('checked');
+		this.updateState();
+	}
+
+	editingItem() {
+		this.element.removeEventListener('click', this.savedListener, false);
+		this.element.classList.remove('checked');
+		this.element.classList.add('editing');
+		this.element.setAttribute('contenteditable', true);
+	}
+
+	savingItem() {
+		this.element.setAttribute('contenteditable', false);
+		this.element.classList.remove('editing');
+		this.name = this.element.textContent;
+		this.element.addEventListener('click', this.savedListener, false);
+
+		this.updateState();
+	}
+
+	deletingItem() {
+		deleteLocalStorageItem(items, this);
+		window.location.reload();
+	}
+
+	updateState() {
+		// QUESTION: why does updating this.state also update the items array without explicitly telling it do so?
+		if(this.element.className.includes('checked')) {
+			// console.log("is this checked?", this.element.className.includes('checked'));
+			// items[this.id].state === true;
+			this.state = true;
+		} else {
+			// console.log("is this checked?", this.element.className.includes('checked'));
+			this.state = false;
+		}
+
+		updateLocalStorage("items", items);
+	}
+}
