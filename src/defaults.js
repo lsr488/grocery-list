0f1f67e9f72288210573aff45aaa051961cc7787
lsr488 <lsr488@gmail.com>
2021-05-15 11:50:26 -0400

    Update defaults.js

--- src/defaults.js
+++ src/defaults.js
@@ -1,52 +1,68 @@
-// 05-09-2021 00:16
+// 05-15-2021 10:49
 // ITEM OUTPUT:
 const defaultItems = [
   {
-    "name": "napkins", 
+    "name": "unsalted butter", 
     "state": false, 
   },
   {
-    "name": "eggs", 
+    "name": "popcorn ", 
     "state": false, 
   },
   {
-    "name": "pancake mix?", 
+    "name": "14 oz can artichoke hearts", 
     "state": false, 
   },
   {
-    "name": "10 oz green beans (orange chicken)", 
+    "name": "capers", 
     "state": false, 
   },
   {
-    "name": "orange juice (orange chicken)", 
+    "name": "1 small can of corn", 
     "state": false, 
   },
   {
-    "name": "1 inch ginger (orange chicken)", 
+    "name": "1 4-oz can chopped green chilies", 
     "state": false, 
   },
   {
-    "name": "sugar/snap peas (orange chicken)", 
+    "name": "1/2 cup sour cream / crema /creme fraiche", 
     "state": false, 
   },
   {
-    "name": "1 red bell pepper (orange chicken)", 
+    "name": "8-10 medium tortillas", 
     "state": false, 
   },
   {
-    "name": "macaroni elbows (mac n cheese)", 
+    "name": "1 shallot", 
     "state": false, 
   },
   {
-    "name": "broccoli (mac n cheese)", 
+    "name": "1 head broccoli", 
     "state": false, 
   },
   {
-    "name": "milk (mac n cheese)", 
+    "name": "10 oz cherry tomatoes", 
     "state": false, 
   },
   {
-    "name": "shredded cheese (mac n cheese)", 
+    "name": "lemons", 
+    "state": false, 
+  },
+  {
+    "name": "clementines", 
+    "state": false, 
+  },
+  {
+    "name": "lime", 
+    "state": false, 
+  },
+  {
+    "name": "1 small onion", 
+    "state": false, 
+  },
+  {
+    "name": "1 red bell pepper", 
     "state": false, 
   },
 ]
@@ -58,13 +74,13 @@ const defaultRecipes = [
     "notes": "",  
   },
   { 
-    "url": "https://www.dropbox.com/s/duppy2aymvj1t5n/WWL%20-%20Tangy%20Orange%20Chicken.pdf?dl=0", 
-    "name": "Tangy Orange Chicken", 
-    "notes": "substituce rice for noodles; add sugar/snap peas;add red bell pepper",  
+    "url": "https://www.dropbox.com/s/vdvdueazjxlukmq/wwl%20-%20pasta%20primavera.pdf?dl=0", 
+    "name": "Pasta Primavera", 
+    "notes": "",  
   },
   { 
-    "url": "https://www.dropbox.com/s/fetrqqke85m4l79/WWL%20-%20Pesto%20Broccoli%20Mac%20n%20Cheese.pdf?dl=0", 
-    "name": "Pesto Broccoli Mac n Cheese", 
+    "url": "https://www.dropbox.com/s/vfy985u21g3v9o6/Adam%20Ragusea%27s%20Lentil%20Tacos%20with%20Crema.pdf?dl=0", 
+    "name": "Adam Ragusea's Lentil Tacos with Crema", 
     "notes": "",  
   },
 ]
