7c1672bb8012265baf306aeaa75d95b74f7ff7bf
lsr488 <lsr488@gmail.com>
2021-09-11 16:25:31 -0400

    Update defaults.js

--- src/defaults.js
+++ src/defaults.js
@@ -1,9 +1,14 @@
-// 09-07-2021 11:00
+// 09-11-2021 04:24
 // RECIPE OUTPUT:
 const defaultRecipes = [
   { 
-    "url": "", 
-    "name": "snack boxes", 
+    "url": "https://docs.google.com/document/d/1clj5ttgboIT0st19qC8GuxhxGfWsnF02o27pQul6DzM/edit", 
+    "name": "Adam Ragusea's Lentil Tacos with Cream", 
+    "notes": "",  
+  },
+  { 
+    "url": "https://wwlprogram.com/#/recipes/301", 
+    "name": "Veggie Chow Mein", 
     "notes": "",  
   },
 ]
@@ -18,31 +23,75 @@ const defaultItems = [
     "state": false, 
   },
   {
-    "name": "olive oil", 
+    "name": "black beans?", 
     "state": false, 
   },
   {
-    "name": "grapes", 
+    "name": "1 small onion", 
     "state": false, 
   },
   {
-    "name": "apples x2", 
+    "name": "1 small red bell pepper", 
     "state": false, 
   },
   {
-    "name": "crackers", 
+    "name": "1 small can of sweet corn", 
     "state": false, 
   },
   {
-    "name": "hummus", 
+    "name": "1 4oz can chopped green chilies", 
     "state": false, 
   },
   {
-    "name": "pita", 
+    "name": "taco seasoning?", 
     "state": false, 
   },
   {
-    "name": "carrots", 
+    "name": "0.5 cup sour cream", 
+    "state": false, 
+  },
+  {
+    "name": "1 lime", 
+    "state": false, 
+  },
+  {
+    "name": "rice?", 
+    "state": false, 
+  },
+  {
+    "name": "pasta?", 
+    "state": false, 
+  },
+  {
+    "name": "8-10 medium tortillas", 
+    "state": false, 
+  },
+  {
+    "name": "soy sauce", 
+    "state": false, 
+  },
+  {
+    "name": "1 inch ginger", 
+    "state": false, 
+  },
+  {
+    "name": "1 bunch green onion", 
+    "state": false, 
+  },
+  {
+    "name": "8 oz mushrooms", 
+    "state": false, 
+  },
+  {
+    "name": "1 head broccoli", 
+    "state": false, 
+  },
+  {
+    "name": "3 medium carrots", 
+    "state": false, 
+  },
+  {
+    "name": "6 oz thin Asian-style egg noodles", 
     "state": false, 
   },
 ]
