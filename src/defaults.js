c7f68558c5e1a629972c6eb9b2d00c2d05943d80
lsr488 <lsr488@gmail.com>
2022-04-25 12:30:03 -0400

    Update defaults.js

--- src/defaults.js
+++ src/defaults.js
@@ -1,61 +1,65 @@
-// 01-12-2022 09:48
+// 04-25-2022 00:29
 // RECIPE OUTPUT:
 const defaultRecipes = [
   { 
-    "url": "https://wwlprogram.com/#/recipes/206", 
-    "name": "sheet pan ratatouille ", 
-    "notes": "beets not eggplant; carrot not onion; add mushrooms ",  
+    "url": "https://wwlprogram.com/#/recipes/488", 
+    "name": "Chicken Ginger Rice Soup", 
+    "notes": "sub paprila for turmeric;",  
   },
   { 
-    "url": "https://wwlprogram.com/#/recipes/328", 
-    "name": "lasagna soup", 
-    "notes": "regular noodles not lasagna noodles",  
+    "url": "https://wwlprogram.com/#/recipes/106", 
+    "name": "Spinach, Pear, and Quinoa Salad", 
+    "notes": "add beets; sub parmesan for feta; sub apples for pears?; skip quinoa",  
   },
 ]
 // ITEM OUTPUT:
 const defaultItems = [
   {
-    "name": "beets", 
+    "name": "1 small yellow onion", 
     "state": false, 
   },
   {
-    "name": "zucchini", 
+    "name": "2 inch gniger", 
     "state": false, 
   },
   {
-    "name": "yellow squash ", 
+    "name": "16 oz chicken thighs", 
     "state": false, 
   },
   {
-    "name": "red bell pepper", 
+    "name": "turmeric", 
     "state": false, 
   },
   {
-    "name": "cherry tomatoes ", 
+    "name": "32 oz chicken stock", 
     "state": false, 
   },
   {
-    "name": "carrot", 
+    "name": "2 small yukon gold potatoes", 
     "state": false, 
   },
   {
-    "name": "mushrooms ", 
+    "name": "2 carrot", 
     "state": false, 
   },
   {
-    "name": "chicken broth", 
+    "name": "3 cup spinach", 
     "state": false, 
   },
   {
-    "name": "zucchini ", 
+    "name": "15 oz chickpeas", 
     "state": false, 
   },
   {
-    "name": "15oz tomato sauce", 
+    "name": "2 large beets", 
     "state": false, 
   },
   {
-    "name": "half and half", 
+    "name": "2 apples", 
+    "state": false, 
+  },
+  {
+    "name": "24 oz spinach", 
     "state": false, 
   },
 ]
